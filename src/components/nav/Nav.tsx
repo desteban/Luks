6db46041be32a5a2fc504c3db29cf841c99deeae
desteban <esteban28ec@outlook.com>
2024-01-29 22:46:53 -0500

    boton para cerrar sesión en el header

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,11 +3,12 @@
 import estilos from './Estilos.module.css'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
-import { UserIcon } from '../../../public/iconos/UserIcon'
 import { useEffect, useState } from 'react'
 import { Session } from 'next-auth'
 import { Button } from '../ui/button'
 import MenuIcon from '@iconos/MenuIcon'
+import SalirIcon from '@iconos/SalirIcon'
+import { UserIcon } from '@iconos/UserIcon'
 
 export function Nav() {
 	const { data } = useSession()
@@ -19,15 +20,17 @@ export function Nav() {
 	}, [data])
 
 	const handleMenu = () => {
-		// alert('estado actual: ' + mostrarMenu)
 		setMostrarMenu(!mostrarMenu)
 	}
 
-	const IniciarSession = () => {
+	const CerrarSession = () => {
+		signOut({ callbackUrl: '/' })
+	}
+
+	const Session = () => {
 		if (session) {
 			return (
-				<div className="flex gap-3 items-center">
-					<p>{session.user?.name}</p>
+				<div className="bg-slate-900 w-full pt-3 px-5 border-t-2 border-gray-50 flex flex-wrap gap-2 items-center pb-3">
 					{session.user?.image ? (
 						<img
 							src={session.user?.image ?? ''}
@@ -35,42 +38,19 @@ export function Nav() {
 							className="rounded-full h-10"
 						/>
 					) : (
-						<UserIcon />
+						<UserIcon size={40} />
 					)}
 
-					<button onClick={() => signOut({ callbackUrl: '/' })}>Salir</button>
+					<div className="">
+						<h2 className="text-white mb-2 ml-0">Hola de regreso</h2>
+						<p>
+							<small>{session.user?.name}</small>
+						</p>
+					</div>
 				</div>
 			)
 		}
 
-		return (
-			<div className="flex gap-3 items-center">
-				{/* <button
-					onClick={() => signIn()}
-					className="bg-sky-400 px-3 py-2 rounded-md"
-				>
-					Iniciar sesión
-				</button> */}
-
-				<Link
-					href={'/registro'}
-					className="bg-sky-400 px-3 py-2 rounded-2xl text-gray-50 text-center"
-				>
-					Crea tu cuenta
-				</Link>
-			</div>
-		)
-	}
-
-	const HandleInicio = () => {
-		if (session) {
-			return <Link href="/inicio">Inicio</Link>
-		}
-
-		return <Link href="/">Inicio</Link>
-	}
-
-	const Session = () => {
 		return (
 			<div className="bg-slate-900 w-full pt-3 px-5 border-t-2 border-gray-50 flex flex-wrap gap-2 items-center">
 				<UserIcon size={40} />
@@ -102,6 +82,26 @@ export function Nav() {
 		)
 	}
 
+	const Salir = () => {
+		if (!session) {
+			return null
+		}
+
+		return (
+			<div className="bg-gray-200 text-black p-5 pt-2 border-t-2 border-zinc-400 flex flex-row-reverse">
+				<Button
+					variant={'outline'}
+					onClick={CerrarSession}
+				>
+					<div className="flex items-center">
+						<SalirIcon />
+						Salir
+					</div>
+				</Button>
+			</div>
+		)
+	}
+
 	return (
 		<header className={estilos.header}>
 			<div className={estilos['header-contenido']}>
@@ -112,7 +112,7 @@ export function Nav() {
 				/>
 			</div>
 
-			<nav className={`rounded-xl ${mostrarMenu ? '' : 'hidden'}`}>
+			<nav className={`rounded-xl ${mostrarMenu ? '' : 'hidden'} mb-5`}>
 				<Session />
 
 				<div className="bg-gray-200 text-black p-5">
@@ -123,14 +123,7 @@ export function Nav() {
 					</ul>
 				</div>
 
-				<div className="bg-gray-200 text-black p-5 pt-2 border-t-2 border-zinc-400">
-					Salidas
-					<ul>
-						<li>Inicio</li>
-						<li>Ingreso</li>
-						<li>Gastos</li>
-					</ul>
-				</div>
+				<Salir />
 			</nav>
 		</header>
 	)
